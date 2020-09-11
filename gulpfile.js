04e8d97287b0498f5729828bcf4a8f8816704508
Marko Karapandzic <webdevmastersensei@gmail.com>
2020-09-11 17:34:37 +0200

    Gulpfile Modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-var { src, dest, series, watch } = require("gulp");
-var sass = require("gulp-sass");
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
-sass.compiler = require("node-sass");
-
-function buildSass() {
+function compileSass() {
   return src("./src/scss/**/*.scss")
-    .pipe(sass().on("error", sass.logError()))
-    .pipe(gulp.dest("./src/css"));
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./src/css"))
+    .pipe(browserSync.stream());
 }
 
 function watchSass() {
-  watch([buildSass()], function (callback) {
-    callback();
+  browserSync.init({
+    server: {
+      baseDir: "./src",
+    },
   });
+  watch("./src/scss/**/*.scss").on("change", compileSass);
+  watch("./src/*.html").on("change", browserSync.reload);
+  watch("./src/js/**/*.js").on("change", browserSync.reload);
 }
 
-exports.default = series(watchSass());
+exports.watch = watchSass;
