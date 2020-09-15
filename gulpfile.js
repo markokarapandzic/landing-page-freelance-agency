d41f159b745a2a4cb58196171ff43753ca444d0a
Marko Karapandzic <webdevmastersensei@gmail.com>
2020-09-15 17:24:15 +0200

    Config Finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require("gulp");
+const fs = require('fs');
+const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass");
 const browserSync = require("browser-sync").create();
+const image = require('gulp-image');
+const webp = require('gulp-webp');
+const clean = require('gulp-clean');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify-es').default;
 
 function compileSass() {
   return src("./src/scss/**/*.scss")
@@ -20,4 +27,47 @@ function watchSass() {
   watch("./src/js/**/*.js").on("change", browserSync.reload);
 }
 
+// Build Config
+function preBuild(next) {
+  if (fs.existsSync('./dist')) {
+    return src('./dist/', { read: false })
+      .pipe(clean());
+  } else {
+    return next();
+  }
+}
+
+function compressImages() {
+  return src('./src/images/*')
+    .pipe(image())
+    .pipe(webp())
+    .pipe(dest('dist/images'));
+}
+
+function minifyCSS() {
+  return src('./src/css/**/*.css')
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(dest('dist/css'));
+}
+
+function minifyJS() {
+  return src('./src/js/*')
+    .pipe(uglify())
+    .pipe(dest('./dist/js'));
+}
+
+function moveIndexHTML() {
+  return src('./src/index.html')
+    .pipe(dest('dist/'));
+}
+
 exports.watch = watchSass;
+exports.build = series(
+  preBuild,
+  compileSass,
+  minifyCSS,
+  minifyJS,
+  compressImages,
+  moveIndexHTML
+);
